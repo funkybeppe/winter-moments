b109cb307dcef94113ccfdb0145096a5983ca760
funkybeppe <lucabarbero1996@gmail.com>
2023-02-06 20:58:03 +0000

    fix typo error

--- wintermoments/settings.py
+++ wintermoments/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'cloudinary_storage',
     'django.contrib.staticfiles',
-    'django.contrib.staticfiles',
     'cloudinary',
     'blog',
 ]
