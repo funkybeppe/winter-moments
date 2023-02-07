446cfa899636fd96738c46f5fa096fc496f39e72
funkybeppe <lucabarbero1996@gmail.com>
2023-02-07 11:24:12 +0000

    add post-detail url

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import Home
+from .views import Home, PostDetailView
 # from . import views
 
 
 urlpatterns = [
     # path('', views.home, name="home"),
     path('', Home.as_view(), name='home'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
 ]
