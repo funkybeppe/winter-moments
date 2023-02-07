a9006e5a53ee074cff0b8eee6bb67f1759b9b9da
funkybeppe <lucabarbero1996@gmail.com>
2023-02-07 12:58:52 +0000

    add addpost view class and fields

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 # def home(request):
@@ -14,3 +14,9 @@ class Home(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = 'post_details.html'
+
+
+class AddPostView(CreateView):
+    model = Post
+    template_name = 'add_post.html'
+    fields = '__all__'
