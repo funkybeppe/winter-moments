abfeea3701ad1e3fa6b7bf4a222b6dcd76821730
funkybeppe <lucabarbero1996@gmail.com>
2023-02-07 13:12:02 +0000

    add crispy form to settings

--- wintermoments/settings.py
+++ wintermoments/settings.py
@@ -45,8 +45,14 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'blog',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
