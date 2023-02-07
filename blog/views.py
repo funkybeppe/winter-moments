f6d1d846e44a6301b468f721ae8b258d82abd3a1
funkybeppe <lucabarbero1996@gmail.com>
2023-02-07 11:03:30 +0000

    add home view for homepage

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
 
-def home(request):
-    return render(request, 'home.html', {})
+# def home(request):
+#     return render(request, 'home.html', {})
+
+class Home(ListView):
+    model = Post
+    template_name = 'home.html'
