6f83dd892b5e8cd5ae8284ac3af226fc2af9940b
funkybeppe <lucabarbero1996@gmail.com>
2023-02-08 10:08:46 +0000

    create url for edit post page and imported view

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import Home, PostDetailView, AddPostView
+from .views import Home, PostDetailView, AddPostView, UpdatePostView
 # from . import views
 
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
     path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
     path('add_post/', AddPostView.as_view(), name='add_post'),
+    path('post/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
 ]
