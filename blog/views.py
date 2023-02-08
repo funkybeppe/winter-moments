4bb7f7c2a962a0784003035b87f4c446242654bd
funkybeppe <lucabarbero1996@gmail.com>
2023-02-08 10:21:59 +0000

    add edit form class for update post page

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, EditForm
 
 # def home(request):
 #     return render(request, 'home.html', {})
@@ -21,10 +21,9 @@ class AddPostView(CreateView):
     model = Post
     form_class = PostForm
     template_name = 'add_post.html'
-    # fields = ('title', 'title_tag', 'author', 'body')
 
 
 class UpdatePostView(UpdateView):
     model = Post
     template_name = 'update_post.html'
-    fields = ['title', 'title_tag', 'body']
+    form_class = EditForm
