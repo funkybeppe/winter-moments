313fc3460d2bcaf9362b66e3e57e122065618175
funkybeppe <lucabarbero1996@gmail.com>
2023-02-09 11:17:28 +0000

    add category model and migrate to db

--- blog/models.py
+++ blog/models.py
@@ -6,13 +6,24 @@ from datetime import datetime, date
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    title_tag = models.CharField(max_length=255, default="Winter Moments")
+    title_tag = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     created_on = models.DateField(auto_now_add=True)
+    category = models.CharField(max_length=255, default="Ski")
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('home')
