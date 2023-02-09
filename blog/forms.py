68fb5e69be69697d4ce6aafb5a5aee3288cc92d9
funkybeppe <lucabarbero1996@gmail.com>
2023-02-09 11:25:03 +0000

    add category field to add post form

--- blog/forms.py
+++ blog/forms.py
@@ -5,12 +5,13 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author', 'body')
+        fields = ('title', 'title_tag', 'author', 'category', 'body')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
         }
 
