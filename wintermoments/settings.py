f4f2ca73efb3a75f0ec426fc1536db425083f54b
funkybeppe <lucabarbero1996@gmail.com>
2023-02-09 10:00:46 +0000

    add login and logout redirection links to home

--- wintermoments/settings.py
+++ wintermoments/settings.py
@@ -52,6 +52,9 @@ INSTALLED_APPS = [
     'members',
 ]
 
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
