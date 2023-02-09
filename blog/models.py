9c2d999ff5e68bbbe9e117e5c3d25e292c4affb6
funkybeppe <lucabarbero1996@gmail.com>
2023-02-09 11:07:00 +0000

    fixed bug for posts unique id

--- blog/models.py
+++ blog/models.py
@@ -15,4 +15,4 @@ class Post(models.Model):
         return self.title + ' | ' + str(self.author)
 
     def get_absolute_url(self):
-        return reverse('post-detail', args=(str(self.id)))
+        return reverse('post-detail', kwargs={'pk': self.pk})
