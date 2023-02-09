b3a28622c45a4005e37c748f0ee1ef78d972c623
funkybeppe <lucabarbero1996@gmail.com>
2023-02-09 11:42:20 +0000

    remove default category and migrate to db

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     created_on = models.DateField(auto_now_add=True)
-    category = models.CharField(max_length=255, default="Ski")
+    category = models.CharField(max_length=255)
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
