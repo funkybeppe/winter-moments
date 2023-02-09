21b0058ffd1d06a9dbc950bb8194452b0cdd8159
funkybeppe <lucabarbero1996@gmail.com>
2023-02-09 11:19:57 +0000

    import category model to admin

--- blog/admin.py
+++ blog/admin.py
@@ -4,5 +4,6 @@ from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
+@admin.register(Category)
 class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
