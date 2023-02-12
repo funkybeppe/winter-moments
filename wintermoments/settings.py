2ed4406e581c80b308669967bab997b1516d63c4
funkybeppe <lucabarbero1996@gmail.com>
2023-02-12 17:00:03 +0000

    deployment commit

--- wintermoments/settings.py
+++ wintermoments/settings.py
@@ -147,9 +147,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_STORAGE = (
-#     'cloudinary_storage.storage.StaticHashedCloudinaryStorage')
-STATICFILES_STORAGE = environ.get('CLOUDINARY_URL')
+STATICFILES_STORAGE = (
+    'cloudinary_storage.storage.StaticHashedCloudinaryStorage')
 STATICFILES_DIRS = [Path.joinpath(BASE_DIR, 'static')]
 STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
 
