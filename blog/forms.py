26cd475f9f49347259ff0472aee58143b178d7da
funkybeppe <lucabarbero1996@gmail.com>
2023-02-12 12:54:16 +0000

    add username placeholder when creating a post

--- blog/forms.py
+++ blog/forms.py
@@ -17,7 +17,9 @@ class PostForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
-            'author': forms.Select(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(
+                attrs={'class': 'form-control', 'id': 'username-field'}),
+            # 'author': forms.Select(attrs={'class': 'form-control'}),
             'category': forms.Select(
                 choices=choice_list, attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
