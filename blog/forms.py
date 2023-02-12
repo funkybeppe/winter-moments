228486ccb21cf77e6da19a5ab09df70c9a6b6842
funkybeppe <lucabarbero1996@gmail.com>
2023-02-12 13:43:51 +0000

    reformat code to solve line lenght problem

--- blog/forms.py
+++ blog/forms.py
@@ -18,7 +18,11 @@ class PostForm(forms.ModelForm):
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
             'author': forms.TextInput(
-                attrs={'class': 'form-control', 'value': '', 'id': 'username-field', 'type': 'hidden'}),
+                attrs={
+                    'class': 'form-control',
+                    'value': '',
+                    'id': 'username-field',
+                    'type': 'hidden'}),
             # 'author': forms.Select(attrs={'class': 'form-control'}),
             'category': forms.Select(
                 choices=choice_list, attrs={'class': 'form-control'}),
