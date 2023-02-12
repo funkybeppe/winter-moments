95d67174ae0624e8f0a853ee376f407ace94232a
funkybeppe <lucabarbero1996@gmail.com>
2023-02-12 15:35:18 +0000

    add init function to style signup form fields

--- members/forms.py
+++ members/forms.py
@@ -4,9 +4,15 @@ from django import forms
 
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField()
-    first_name = forms.CharField(max_length=50)
-    last_name = forms.CharField(max_length=50)
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'class': 'form-control'})
+        )
+    first_name = forms.CharField(
+        max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'})
+        )
+    last_name = forms.CharField(
+        max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'})
+        )
 
     class Meta:
         model = User
@@ -18,3 +24,10 @@ class SignUpForm(UserCreationForm):
             'password1',
             'password2'
         )
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
