063bac65d5398c68251be1b4a470d254871be27b
funkybeppe <lucabarbero1996@gmail.com>
2023-02-12 11:31:25 +0000

    add context data function to postdetailview to show category menu

--- blog/views.py
+++ blog/views.py
@@ -25,6 +25,12 @@ class PostDetailView(DetailView):
     model = Post
     template_name = 'post_details.html'
 
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(PostDetailView, self).get_context_data(*args, **kwargs)
+        context['cat_menu'] = cat_menu
+        return context
+
 
 class AddPostView(CreateView):
     model = Post
