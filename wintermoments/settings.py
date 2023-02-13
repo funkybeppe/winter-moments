3a020f9a648f5da60fbc89391eebb90a12738eed
funkybeppe <lucabarbero1996@gmail.com>
2023-02-13 15:12:09 +0000

    deployment commit

--- wintermoments/settings.py
+++ wintermoments/settings.py
@@ -150,14 +150,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = (
-    'cloudinary_storage.storage.StaticHashedCloudinaryStorage')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
-
-
 MEDIA_URL = '/media/'
+
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CLOUDINARY_STORAGE = {
