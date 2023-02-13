299e6f086ce35ee438f4539697aaba3266924264
funkybeppe <lucabarbero1996@gmail.com>
2023-02-13 15:41:39 +0000

    create usereditview and import userchangeform

--- members/views.py
+++ members/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
 from .forms import SignUpForm
 
@@ -9,3 +9,9 @@ class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
     template_name = 'registration/signup.html'
     success_url = reverse_lazy('login')
+
+
+class UserEditView(generic.CreateView):
+    form_class = UserChangeForm()
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('home')
