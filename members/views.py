de18c675c02841045098e49696379dbec50f977e
funkybeppe <lucabarbero1996@gmail.com>
2023-02-13 16:05:21 +0000

    import editprofileform

--- members/views.py
+++ members/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
-from .forms import SignUpForm
+from .forms import SignUpForm, EditProfileForm
 
 
 class UserRegisterView(generic.CreateView):
@@ -12,7 +12,7 @@ class UserRegisterView(generic.CreateView):
 
 
 class UserEditView(generic.UpdateView):
-    form_class = UserChangeForm
+    form_class = UserProfileForm
     template_name = 'registration/edit_profile.html'
     success_url = reverse_lazy('home')
 
