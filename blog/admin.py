9fb3a8e505b3e5b88abe37ded8de08b14d5583a6
funkybeppe <lucabarbero1996@gmail.com>
2023-02-16 10:42:46 +0000

    deployment commit

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Post, Category
+from .models import Post, Category, Profile
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
 @admin.register(Category)
+@admin.register(Profile)
 class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
--- blog/models.py
+++ blog/models.py
@@ -34,3 +34,15 @@ class Category(models.Model):
 
     def get_absolute_url(self):
         return reverse('home')
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = models.TextField()
+    profile_picture = CloudinaryField('image', blank=True, null=True)
+    facebook_link = models.CharField(max_length=255, null=True)
+    instagram_link = models.CharField(max_length=255, null=True)
+    twitter_link = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        return str(self.user)
