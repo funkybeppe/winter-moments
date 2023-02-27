78ad64d5f9ee7c6b933d90312e98b3d35e99b1e8
funkybeppe <lucabarbero1996@gmail.com>
2023-02-27 22:42:00 +0000

    set debug to false

--- wintermoments/settings.py
+++ wintermoments/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['winter-moments.herokuapp.com', 'localhost', '127.0.0.1']
 
