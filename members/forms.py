a05b427f990222bd5def6dac069fde047574bbfb
funkybeppe <lucabarbero1996@gmail.com>
2023-02-27 22:21:32 +0000

    deployment commit

--- members/forms.py
+++ members/forms.py
@@ -4,10 +4,7 @@ from django.contrib.auth.models import User
 from django import forms
 from blog.models import Profile
 from django.contrib.auth.forms import (
-                                        UserCreationForm,
-                                        UserChangeForm,
-                                        PasswordChangeForm
-                                    )
+            UserCreationForm, UserChangeForm, PasswordChangeForm)
 
 
 class PasswordChangingForm(PasswordChangeForm):
