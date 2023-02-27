fa63700b12836320318bae3d7a15ecf1d9af55ff
funkybeppe <lucabarbero1996@gmail.com>
2023-02-27 19:28:54 +0000

    add comments and reformat code

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,4 @@
+// CREDIT: function to close messages after 2 seconds taken from Code Institute
 setTimeout(function () {
     let messages = document.getElementById('msg');
     let alert = new bootstrap.Alert(messages);
