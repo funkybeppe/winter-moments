6dc33243408ead60893eb03970d91bdeb982a03e
funkybeppe <lucabarbero1996@gmail.com>
2023-02-27 17:20:42 +0000

    remove settings

--- wintermoments/settings.py
+++ wintermoments/settings.py
@@ -104,17 +104,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'wintermoments.wsgi.application'
 
-if "DATABASE_URL" in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-        }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
